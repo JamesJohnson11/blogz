dc85394aa191c155187c7d27a9daca56c64c08b8
Barry Johnson <bdotmgmt@gmail.com>
2018-01-16 09:44:09 -0500

    build-a-blog part way done

--- main.py
+++ main.py
@@ -3,15 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello11@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'hello1111'
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(240))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    return render_template('blog.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+    
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def index():
-    
     if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+
+        blogs = Blog.query.all()
+        blogs.append(new_blog)
         return redirect('/blog')
 
     return render_template('new_post.html')
